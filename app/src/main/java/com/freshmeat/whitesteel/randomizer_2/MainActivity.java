7e550410a23757585899ba29e131fb12afdcca00
Whitesteel <amaterasusan96@gmail.com>
2015-07-24 22:20:35 +0600

    deleted Wide-mode

--- app/src/main/java/com/freshmeat/whitesteel/randomizer_2/MainActivity.java
+++ app/src/main/java/com/freshmeat/whitesteel/randomizer_2/MainActivity.java
@@ -27,110 +27,48 @@ public class MainActivity extends FragmentActivity implements NoticeDialogFragme
 
 
     private HD_Resol_btn hd_resol_btn;
-    private Wide_Resol_btn wide_resol_btn;
     private GenresHD genresHD;
-    private GenresWide genresWide;
-
     final String LOG_TAG = "mLOGs";
 
     private FragmentManager manager;
     private FragmentTransaction transaction;
 
-    public Switch aSwitch;
     private SharedPreferences.Editor editor;
     public static final String APP_PREFERENCE = "mySettings";
     public static final String HD_Switchers = "HD_Switcher";
-    public static final String Wide_Switchers = "Wide_Switcher";
+//    public static final String Wide_Switchers = "Wide_Switcher";
     public static final String HD_Resolution = "HD_Walls";
-    public static final String Wide_Resolution = "WIDE_Walls";
+//    public static final String Wide_Resolution = "WIDE_Walls";
     private SharedPreferences mSettings;
     public Intent startIntent;
     String str1;
-    String str2;
-    int a;
+
     Bundle bundle = new Bundle();
 
-    boolean addList = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        aSwitch = (Switch) findViewById(R.id.switch1);
+
         hd_resol_btn = new HD_Resol_btn();
-        wide_resol_btn = new Wide_Resol_btn();
+
         manager = getSupportFragmentManager();
         genresHD = new GenresHD();
-        genresWide = new GenresWide();
+//        genresWide = new GenresWide();
 
-        startIntent = new Intent(Intent.ACTION_MAIN);
-        startIntent.addCategory(Intent.CATEGORY_HOME);
-        startIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        startIntent = new Intent(Intent.ACTION_MAIN);
+//        startIntent.addCategory(Intent.CATEGORY_HOME);
+//        startIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-
-        transaction = manager.beginTransaction();
-
-//        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-//                if(getIntent().hasExtra("800")){
-//                    aSwitch.setChecked(true);
-//                    transaction.add(R.id.hdWideContainer, wide_resol_btn, Wide_Resol_btn.TAG);
-//                    transaction.add(R.id.genres_container, genresWide, GenresWide.TAG);
-//                    Log.d(LOG_TAG,"Ne Huinya");
-//                }
-//                else {
-//                    Log.d(LOG_TAG, "Huinya");
-//
-//                }
-//                transaction.commit();
-//
-//            }
-//        });
         mSettings = getSharedPreferences(APP_PREFERENCE, Context.MODE_PRIVATE);
 
     }
 
-    public void onClickFragmentHDorWide1(View view) {
-
-        transaction = manager.beginTransaction();
-
-        switch (view.getId()) {
-
-            case R.id.switch1:
-                if (manager.findFragmentByTag(hd_resol_btn.TAG) != null) {
-                    transaction.replace(R.id.hdWideContainer, wide_resol_btn, Wide_Resol_btn.TAG);
-                    editor = mSettings.edit();
-                    editor.putString(Wide_Switchers, "Wide");
-            //        aSwitch.setChecked(true);
-                    Log.d(LOG_TAG, "editor, putString Wide");
-                    editor.apply();
-                }
-                if (manager.findFragmentByTag(wide_resol_btn.TAG) != null){
-                    transaction.replace(R.id.hdWideContainer, hd_resol_btn, HD_Resol_btn.TAG);
-                    editor = mSettings.edit();
-                    editor.putString(HD_Switchers, "HD");
-          //          aSwitch.setChecked(false);
-                    Log.d(LOG_TAG, "editor, putString HD");
-                    editor.apply();
-                }
-                if (manager.findFragmentByTag(genresHD.TAG) != null){
-                    transaction.replace(R.id.genres_container, genresWide, GenresWide.TAG);
-                }
-                if(manager.findFragmentByTag(genresWide.TAG) != null){
-                    transaction.replace(R.id.genres_container, genresHD, GenresHD.TAG);
-                }
-                break;
-            }
-
-        transaction.commit();
-
-    }
 
     @Override
     protected void onResume() {
 
         transaction = manager.beginTransaction();
-
 
         if(!bundle.getBoolean("True",false)){
             str1 = mSettings.getString(HD_Switchers, "HD");
@@ -138,37 +76,16 @@ public class MainActivity extends FragmentActivity implements NoticeDialogFragme
             transaction.add(R.id.genres_container, genresHD, GenresHD.TAG);
             Log.d(LOG_TAG,str1 +" sdfsd");
         }
-        else if(bundle.getBoolean("False", true)){
-            str2 = mSettings.getString(Wide_Switchers,"Wide");
-            transaction.add(R.id.hdWideContainer, wide_resol_btn, Wide_Resol_btn.TAG);
-            transaction.add(R.id.genres_container, genresWide, GenresWide.TAG);
-            aSwitch.isChecked();
-            Log.d(LOG_TAG, str2+"  sdfsd");
-        }
         transaction.commit();
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        this.addList = manager.getFragments().isEmpty();
-            if(aSwitch.isChecked()){
-                bundle.putBoolean("False", true);
-
-                Log.d(LOG_TAG, "Skotina");
-
-            }else if(!aSwitch.isChecked()) {
-                bundle.putBoolean("True", false);
-                Log.d(LOG_TAG,"Suka");
-
-            }
-            transaction = manager.beginTransaction();
-            transaction.remove(this.hd_resol_btn);
-            transaction.remove(this.genresHD);
-            transaction.remove(this.wide_resol_btn);
-            transaction.remove(this.genresWide);
-            transaction.commit();
-
+        transaction = manager.beginTransaction();
+        transaction.remove(this.hd_resol_btn);
+        transaction.remove(this.genresHD);
+        transaction.commit();
         super.onPause();
     }
 
@@ -204,29 +121,8 @@ public class MainActivity extends FragmentActivity implements NoticeDialogFragme
 
                 }
                 break;
-            case R.id.genres_go_wide_btn:
-                DialogFragment dialog2 = new NoticeDialogFragmentWide();
-                if(getIntent().hasExtra("800")) {
-                    dialog2.show(getSupportFragmentManager(), "NoticeDialogFragmentWide");
-                }
-                if(getIntent().hasExtra("1050")){
-                    dialog2.show(getSupportFragmentManager(), "NoticeDialogFragment2");
-
-                }
-                if(getIntent().hasExtra("1200")){
-                    dialog2.show(getSupportFragmentManager(), "NoticeDialogFragment3");
-
-                }
-                if(getIntent().hasExtra("1600")){
-                    dialog2.show(getSupportFragmentManager(), "NoticeDialogFragment4");
-
-                }
-
-                break;
         }
     }
-
-
     @Override
     public void onBackPressed() {
 
@@ -242,4 +138,5 @@ public class MainActivity extends FragmentActivity implements NoticeDialogFragme
             Log.d(LOG_TAG,"Pabotaet BblXoD!!");
         }
     }
+
 }
